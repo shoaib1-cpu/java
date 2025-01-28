08561f41ce0e7dc3c7e2f46cd28bbf05ad33c13f
Md Shoaib Alam <goodteacher08@gmail.com>
2025-01-28 16:17:11 +0530

    Update SelectionSort.java

--- CoreJavaProjects/sorting/SelectionSort.java
+++ CoreJavaProjects/sorting/SelectionSort.java
@@ -1,6 +1,1 @@
-Data Structures and Algorithms in Java
-Implement commonly used algorithms (sorting, searching) and data structures (linked lists, trees, graphs) with clear documentation.
-Custom Utilities Library
-A library of reusable Java utilities, such as file parsers, string manipulation tools, or date/time utilities.
-Java Design Patterns
-Examples of implementing design patterns like Singleton, Factory, Observer, etc., with real-world use cases.
+
